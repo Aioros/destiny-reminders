70a6c1ec0098a9632fc2c5cd038a01feb36f41f2
Aioros <aioros83@gmail.com>
2020-09-12 17:37:43 -0400

    First Cloud SQL config

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql2/promise');
+
 const config = {
-    host: process.env.DBHOST,
-	port: process.env.DBPORT,
 	user: process.env.DBUSER,
 	password: process.env.DBPASSWORD,
 	database: process.env.DB,
 	timezone: "Z"
+};
+
+if (process.env.ENVIRONMENT == "local") {
+    config.host = process.env.DBHOST;
+	config.port = process.env.DBPORT;
+} else {
+	config.socketPath = "/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`";
 }
 
 const pool = mysql.createPool(config);
