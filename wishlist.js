be8e2b09a70581e762cec9a2483b0afa43adfcbb
Aioros <aioros83@gmail.com>
2021-05-28 09:49:12 -0400

    Updated with new Lost Sectors

--- wishlist.js
+++ wishlist.js
@@ -47,24 +47,21 @@ module.exports = () => ({
 				]}
 			],
 			sector: [
-				{name: "k1 revelation", neededFor: []},
 				{name: "concealed void", neededFor: [{type: "objective", name: "Europa Lost Sectors", value: false}]},
 				{name: "bunker E15", neededFor: [{type: "objective", name: "Europa Lost Sectors", value: false}]},
 				{name: "perdition", neededFor: [{type: "objective", name: "Europa Lost Sectors", value: false}]},
+				{name: "the quarry", neededFor: []},
+				{name: "scavenger's den", neededFor: []},
+				{name: "excavation site XII", neededFor: []},
 				{name: "exodus garden 2A", neededFor: []},
 				{name: "veles labyrinth", neededFor: []},
+				{name: "the empty tank", neededFor: []},
 				{name: "K1 logistics", neededFor: []},
 				{name: "K1 communion", neededFor: []},
-				{name: "k1 crew quarters", neededFor: []}
-				
+				{name: "k1 crew quarters", neededFor: []},
+				{name: "k1 revelation", neededFor: []}				
 			],
 			reward: [
-				{name: "head", neededFor: [
-					{type: "item", name: "dawn chorus", value: false},
-					{type: "item", name: "precious scars", value: false},
-					{type: "item", name: "mask of bakris", value: false}
-				]},
-				{name: "legs"},
 				{name: "arms", neededFor: [
 					{type: "item", name: "necrotic grip", value: false},
 					{type: "item", name: "icefall mantle", value: false},
@@ -74,6 +71,16 @@ module.exports = () => ({
 					{type: "item", name: "mantle of battle harmony", value: false},
 					{type: "item", name: "cuirass of the falling star", value: false},
 					{type: "item", name: "omnioculus", value: false}
+				]},
+				{name: "head", neededFor: [
+					{type: "item", name: "dawn chorus", value: false},
+					{type: "item", name: "precious scars", value: false},
+					{type: "item", name: "mask of bakris", value: false}
+				]},
+				{name: "legs", neededFor: [
+					{type: "item", name: "boots of the assembler", value: false},
+					{type: "item", name: "the path of burning steps", value: false},
+					{type: "item", name: "star-eater scales", value: false}
 				]}
 			]
 		},
